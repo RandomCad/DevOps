6f5ce3539702238f215bd3076f2a4fd4727add3d
typingMathew <matteo.o.1106@gmail.com>
2025-02-28 11:06:20 +0100

    fix database-connection: - updated database.py to establish working connections (with session closing) to make persistent changes - fixed an error, where an int caused an TypeError in database.py>read_note() - formatted both files with ruff instead of autopep8

--- fuchs/fuchs/database.py
+++ fuchs/fuchs/database.py
@@ -1,5 +1,6 @@
 """This module provides a class to connect to a PostgreSQL database and
 provides methods to write, read and update notes"""
+
 import psycopg2
 
 
@@ -9,122 +10,121 @@ class DatabaseConnection:
 
     def __init__(self, dbname: str, user: str, password: str, host: str):
         self.conn = psycopg2.connect(
-            dbname=dbname,
-            user=user,
-            password=password,
-            host=host
+            dbname=dbname, user=user, password=password, host=host
         )
-
-    def __enter__(self):
-        return self
-
-    def __exit__(self, exc_type, exc_val, exc_tb):
-        if exc_type is not None:
-            self.conn.rollback()
-        else:
-            self.conn.commit()
-        self.conn.close()
 
     def read_note(self, note_id: int):
         """reads a note from the database by id,
         returns a tuple with the title and content of the note"""
-        with self.conn.cursor() as cur:
-            cur.execute(
-                "SELECT note_title, note_content, note_path FROM notes WHERE note_id = %s",
-                (note_id)
-            )
-            ret = cur.fetchone()
-            cur.close()
-            return ret
+        with self.conn:
+            with self.conn.cursor() as cur:
+                cur.execute(
+                    "SELECT note_title, note_content, note_path FROM notes WHERE note_id = %s",
+                    (note_id,),
+                )
+                ret = cur.fetchone()
+        return ret
 
     def read_all_notes(self):
         """reads all notes from the database,
-        returns a list of tuples with the title and content of the notes"""
-        with self.conn.cursor() as cur:
-            cur.execute("SELECT note_id, note_title, note_content, note_path FROM notes")
-            ret = cur.fetchall()
-            cur.close()
-            return ret
+        returns a list of tuples with the title and id of the notes"""
+        with self.conn:
+            with self.conn.cursor() as cur:
+                cur.execute("SELECT note_id, note_title FROM notes")
+                ret = cur.fetchall()
+        return ret
 
     def write_note(self, title: str, content: str, path: str = ""):
         """writes a new note to the database,
         returns the id of the new note"""
-        with self.conn.cursor() as cur:
-            cur.execute(
-                "INSERT INTO notes (note_title, note_content, note_path) VALUES (%s, %s, %s) RETURNING note_id",
-                (title, content, path)
-            )
-            ret = cur.fetchone()[0]
-            cur.close()
-            return ret
+        with self.conn:
+            with self.conn.cursor() as cur:
+                cur.execute(
+                    "INSERT INTO notes (note_title, note_content, note_path) VALUES (%s, %s, %s) RETURNING note_id",
+                    (title, content, path),
+                )
+                ret = cur.fetchone()[0]
+        return ret
 
     def update_note(self, note_id: int, title: str, content: str, path: str):
         """updates a note in the database by id,
         returns the number of rows affected"""
-        with self.conn.cursor() as cur:
-            cur.execute(
-                "UPDATE notes SET note_title = %s, note_content = %s, note_path = %s WHERE note_id = %s",
-                (title, content, path, note_id)
-            )
-            return cur.rowcount
+        with self.conn:
+            with self.conn.cursor() as cur:
+                cur.execute(
+                    "UPDATE notes SET note_title = %s, note_content = %s, note_path = %s WHERE note_id = %s",
+                    (title, content, path, note_id),
+                )
+                ret = cur.rowcount
+        return ret
 
     def remove_note(self, note_id: int):
         """removes a note from the database by id,
         returns the number of rows affected"""
-        with self.conn.cursor() as cur:
-            cur.execute(
-                "DELETE FROM notes WHERE note_id = %s",
-                (note_id)
-            )
-            return cur.rowcount
+        with self.conn:
+            with self.conn.cursor() as cur:
+                cur.execute("DELETE FROM notes WHERE note_id = %s", (note_id,))
+                ret = cur.rowcount
+        return ret
 
-    def store_meta_of_picture(self, note_id: int, pic_name: str, pic_alt_text: str, pic_path: str):
+    def store_meta_of_picture(
+        self, note_id: int, pic_name: str, pic_alt_text: str, pic_path: str
+    ):
         """stores the reference between a piv and a note,
         returns the id of the picture"""
-        with self.conn.cursor() as cur:
-            cur.execute(
-                "INSERT INTO pictures (note_id, picture_name, picture_alt_text, picture_path) VALUES (%s, %s, %s, %s) RETURNING picture_id",
-                (note_id, pic_name, pic_alt_text, pic_path)
-            )
-            return cur.fetchone()[0]
+        with self.conn:
+            with self.conn.cursor() as cur:
+                cur.execute(
+                    "INSERT INTO pictures (note_id, picture_name, picture_alt_text, picture_path) VALUES (%s, %s, %s, %s) RETURNING picture_id",
+                    (note_id, pic_name, pic_alt_text, pic_path),
+                )
+                ret = cur.fetchone()[0]
+        return ret
 
     def read_meta_of_picture(self, pic_id: int):
         """reads the meta information of a picture by id,
         returns a tuple with the note_id, name and alt_text of the picture"""
-        with self.conn.cursor() as cur:
-            cur.execute(
-                "SELECT (note_id, picture_name, picture_alt_text, picture_path) FROM pictures WHERE picture_id = %s",
-                (pic_id)
-            )
-            return cur.fetchone()
+        with self.conn:
+            with self.conn.cursor() as cur:
+                cur.execute(
+                    "SELECT note_id, picture_name, picture_alt_text, picture_path FROM pictures WHERE picture_id = %s",
+                    (pic_id,),
+                )
+                ret = cur.fetchone()
+        return ret
 
     def remove_meta_of_picture(self, pic_id: int):
         """removes a picture from the database by id,
         returns the number of rows affected"""
-        with self.conn.cursor() as cur:
-            cur.execute(
-                "DELETE FROM pictures WHERE picture_id = %s",
-                (pic_id)
-            )
-            return cur.rowcount
+        with self.conn:
+            with self.conn.cursor() as cur:
+                cur.execute("DELETE FROM pictures WHERE picture_id = %s", (pic_id,))
+                ret = cur.rowcount
+        return ret
 
     def read_all_meta_of_pictures(self, note_id: int):
         """reads all pictures of a note by id,\n
         returns a list of tuples with the id, name, alt_text and path of the
         pictures"""
-        with self.conn.cursor() as cur:
-            cur.execute(
-                "SELECT (picture_id, picture_name, picture_alt_text, picture_path) FROM pictures WHERE note_id = %s",
-                (note_id)
-            )
-            return cur.fetchall()
+        with self.conn:
+            with self.conn.cursor() as cur:
+                cur.execute(
+                    "SELECT picture_id, picture_name, picture_alt_text, picture_path FROM pictures WHERE note_id = %s",
+                    (note_id,),
+                )
+                ret = cur.fetchall()
+        return ret
 
-    def update_meta_of_picture(self, pic_id: int, pic_name: str, pic_alt_text: str, pic_path: str):
+    def update_meta_of_picture(
+        self, pic_id: int, pic_name: str, pic_alt_text: str, pic_path: str
+    ):
         """updates the meta information of a picture by id,
         returns the number of rows affected"""
-        with self.conn.cursor() as cur:
-            cur.execute(
-                "UPDATE pictures SET (picture_name = %s, picture_alt_text = %s, picture_path = %s) WHERE picture_id = %s",
-                (pic_name, pic_alt_text, pic_path, pic_id)
-            )
-            return cur.rowcount
+        with self.conn:
+            with self.conn.cursor() as cur:
+                cur.execute(
+                    "UPDATE pictures SET (picture_name = %s, picture_alt_text = %s, picture_path = %s) WHERE picture_id = %s",
+                    (pic_name, pic_alt_text, pic_path, pic_id),
+                )
+                ret = cur.rowcount
+        return ret
--- fuchs/fuchs/main.py
+++ fuchs/fuchs/main.py
@@ -1,31 +1,38 @@
 """defining the API endpoints for the notes application"""
+
 from fastapi import FastAPI, Depends
 import requests
 
-from fuchs import DatabaseConnection, URL_HAMSTER, DB_NAME, DB_USER, \
-DB_PASSWORD, DB_HOST  # , URL_CHAMELEON
+from fuchs import (
+    DatabaseConnection,
+    URL_HAMSTER,
+    DB_NAME,
+    DB_USER,
+    DB_PASSWORD,
+    DB_HOST,
+)  # , URL_CHAMELEON
 
 app = FastAPI()
 
+
 def get_db():
     """returns a database connection"""
-    db = DatabaseConnection(dbname=DB_NAME, user=DB_USER, password=DB_PASSWORD, host=DB_HOST)
+    db = DatabaseConnection(
+        dbname=DB_NAME, user=DB_USER, password=DB_PASSWORD, host=DB_HOST
+    )
     try:
         yield db
     finally:
         db.conn.close()
 
+
 @app.get("/notes")
 def read_all_notes(db: DatabaseConnection = Depends(get_db)):
     """returns a list of all notes"""
     notes = db.read_all_notes()
-    ret = {
-        "notes": [
-            {"id": note[0], "title": note[1], "content": note[2]}
-            for note in notes
-        ]
-    }
+    ret = {"notes": [{"id": note[0], "title": note[1]} for note in notes]}
     return ret
+
 
 @app.get("/notes/{note_id}")
 def read_note(note_id: int, db: DatabaseConnection = Depends(get_db)):
@@ -33,14 +40,14 @@ def read_note(note_id: int, db: DatabaseConnection = Depends(get_db)):
     of the note"""
     note_content_md = db.read_note(note_id)
     pic_data = db.read_all_meta_of_pictures(note_id)
-    pics = [
-        {"id": pic[0], "name": pic[1], "alt_text": pic[2]}
-        for pic in pic_data
-    ]
+    pics = [{"id": pic[0], "name": pic[1], "alt_text": pic[2]} for pic in pic_data]
     return {"id": note_id, "content": note_content_md, "pictures": pics}
 
+
 @app.post("/notes/")
-def create_note(note_title: str, note_content_md: str, db: DatabaseConnection = Depends(get_db)):
+def create_note(
+    note_title: str, note_content_md: str, db: DatabaseConnection = Depends(get_db)
+):
     """creates a new note.\n
     takes the markdown content of the note as input, converts it to html and
     stores the markdown in the db and distributes the html over the hamster.\n
@@ -63,14 +70,19 @@ def create_note(note_title: str, note_content_md: str, db: DatabaseConnection = 
     assert r.status_code == 200
 
     # safe the path to the note in the db
-    affected_rows = db.update_note(
-        note_id, note_title, note_content_md, note_path)
+    affected_rows = db.update_note(note_id, note_title, note_content_md, note_path)
     assert affected_rows == 1, f"affected_rows: {affected_rows}"
 
     return {"status": "created", "id": note_id, "path": note_path}
 
+
 @app.put("/notes/{note_id}")
-def update_note(note_id: int, note_title: str, note_content_md: str, db: DatabaseConnection = Depends(get_db)):
+def update_note(
+    note_id: int,
+    note_title: str,
+    note_content_md: str,
+    db: DatabaseConnection = Depends(get_db),
+):
     """updates the content & title of a note.\n
     takes the new markdown content of the note as input, converts it to html,
     updates the old markdown (on db) and html (on hamster).\n"""
@@ -79,8 +91,7 @@ def update_note(note_id: int, note_title: str, note_content_md: str, db: Databas
 
     note_path = f"note_{note_id}"
     # safe the markdown
-    affected_rows = db.update_note(
-        note_id, note_title, note_content_md, note_path)
+    affected_rows = db.update_note(note_id, note_title, note_content_md, note_path)
     assert affected_rows == 1, f"affected_rows: {affected_rows}"
 
     note_path = f"note_{note_id}"
@@ -89,9 +100,9 @@ def update_note(note_id: int, note_title: str, note_content_md: str, db: Databas
     r = requests.put(url, data=note_content_html, timeout=10)
 
     assert r.status_code == 200
-    assert note_path == r.json()["path"]
 
     return {"status": "updated", "id": note_id}
+
 
 @app.delete("/notes/{note_id}")
 def delete_note(note_id: int, db: DatabaseConnection = Depends(get_db)):
@@ -106,12 +117,18 @@ def delete_note(note_id: int, db: DatabaseConnection = Depends(get_db)):
     # delete the html on the hamster
     r = requests.delete(url, timeout=10)
     assert r.status_code == 200
-    assert note_path == r.json()["path"]
 
     return {"status": "deleted"}
 
+
 @app.post("/notes/{note_id}/pics/")
-def store_picture(note_id: int, pic_content: str, pic_name: str, pic_alt_text: str, db: DatabaseConnection = Depends(get_db)):
+def store_picture(
+    note_id: int,
+    pic_content: str,
+    pic_name: str,
+    pic_alt_text: str,
+    db: DatabaseConnection = Depends(get_db),
+):
     """stores a picture on the hamster.\n
     returns the path to the picture."""
     # TODO: get rid of multiple implementation of note_path- & pic_path-generation
@@ -120,13 +137,20 @@ def store_picture(note_id: int, pic_content: str, pic_name: str, pic_alt_text: s
     r = requests.put(url, data=pic_content, timeout=10)
     assert r.status_code == 200
 
-    pic_id = db.store_meta_of_picture(
-        note_id, pic_name, pic_alt_text, pic_path)
+    pic_id = db.store_meta_of_picture(note_id, pic_name, pic_alt_text, pic_path)
 
     return {"status": "created", "id": pic_id, "path": pic_path}
 
-@app.put("/notes/{note_id}/pics/{pic_path}")
-def update_picture(note_id: int, pic_id: int, pic_content: str, pic_name: str, pic_alt_text: str, db: DatabaseConnection = Depends(get_db)):
+
+@app.put("/notes/{note_id}/pics/{pic_id}")
+def update_picture(
+    note_id: int,
+    pic_id: int,
+    pic_content: str,
+    pic_name: str,
+    pic_alt_text: str,
+    db: DatabaseConnection = Depends(get_db),
+):
     """updates a picture on the hamster.\n
     takes the new picture content as input and updates the picture on the
     hamster."""
@@ -136,13 +160,13 @@ def update_picture(note_id: int, pic_id: int, pic_content: str, pic_name: str, p
     r = requests.put(url, data=pic_content, timeout=10)
     assert r.status_code == 200
 
-    affected_rows = db.update_meta_of_picture(
-        pic_id, pic_name, pic_alt_text, pic_path)
+    affected_rows = db.update_meta_of_picture(pic_id, pic_name, pic_alt_text, pic_path)
     assert affected_rows == 1
 
     return {"status": "updated", "path": pic_path}
 
-@app.delete("/notes/{note_id}/pics/{pic_path}")
+
+@app.delete("/notes/{note_id}/pics/{pic_id}")
 def delete_pic(pic_id: int, pic_path: str, db: DatabaseConnection = Depends(get_db)):
     """deletes a picture and its metadata."""
     affected_rows = db.remove_meta_of_picture(pic_id)
@@ -153,6 +177,7 @@ def delete_pic(pic_id: int, pic_path: str, db: DatabaseConnection = Depends(get_
     assert r.status_code == 200
 
     return {"status": "deleted"}
+
 
 def convert_md_to_html(md: str) -> str:
     """converts markdown to html"""
