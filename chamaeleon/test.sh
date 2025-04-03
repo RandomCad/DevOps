720b30f281e19d7b1f82a787576348c17200c969
ba-lindner <147381263+ba-lindner@users.noreply.github.com>
2025-04-03 22:35:41 +0200

    extend chamaeleon
    - enable gfm
    - wrap converted content in proper html document with css

--- chamaeleon/test.sh
+++ chamaeleon/test.sh
@@ -1,25 +1,39 @@
-if [[ $1 = "docker" ]]; then
-    docker run -d -p 8000:8000 --name chamaeleon_test chamaeleon:test
+# setup
+kind=$1
+if [[ $kind = "docker" ]]; then
+    docker run -d --rm -p 8000:8000 --name chamaeleon_test chamaeleon:test
 else
-    target/debug/chamaeleon &
+    target/debug/chamaeleon.exe &
     pid=$!
 fi
 
 sleep 1
 
-converted=$( \
-curl -X POST http://localhost:8000 \
-    -H "Content-Type: text/plain" \
-    -d "## Hello *world*!")
+finish() {
+    if [[ $kind = "docker" ]]; then
+        docker stop chamaeleon_test -t 3
+    else
+        kill $pid
+    fi
+}
 
-if [[ $1 = "docker" ]]; then
-    docker stop chamaeleon_test -t 3
-else
-    kill $pid
-fi
+exec_test() {
+    converted=$( \
+    curl -X POST http://localhost:8000 \
+        -s -H "Content-Type: text/plain" \
+        --data-binary "@chamaeleon/test_data/inp_$1.txt" \
+    | sed "s/^[[:blank:]]*//;s/[[:blank:]]*$//")
+    expected=$(cat "chamaeleon/test_data/out_$1.txt" | sed "s/^[[:blank:]]*//;s/[[:blank:]]*$//")
+    if [[ $converted != $expected ]]; then
+        echo -e "expected:\n$expected"
+        echo -e "got:\n$converted"
+        finish
+        exit 1
+    fi
+}
 
-if [[ $converted != "<h2>Hello <em>world</em>!</h2>" ]]; then
-    exit 1
-fi
+exec_test basic
+exec_test table
 
+finish
 exit 0
